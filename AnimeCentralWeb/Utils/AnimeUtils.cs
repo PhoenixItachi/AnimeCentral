2ecfca67bf0f87d33527d393a53a660aad7468ad
PhoenixItachi <juventus09deva@yahoo.com>
2017-08-21 18:00:03 +0300

    - Added filters on AnimeList.

---
+++ AnimeCentralWeb/Utils/AnimeUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AnimeCentralWeb.Utils
+{
+    public static class AnimeUtils
+    {
+        public static List<string> Genres = new List<string>() { "Comedie", "Actiune", "Horror", "Romantic", "Istoric",
+                                                                 "Fantezie", "Shounen", "Demoni", "Ecchi", "Mister", "Drama",
+                                                                 "Harem", "Aventura", "Masini", "Dementia", "Joc", "Josei", "Copii", "Magie",
+                                                                 "Arte Martiale", "Mecha", "Muzica", "Parodie", "Politie", "Samurai", "Scoala",
+                                                                 "Sci-Fi", "Shoujo", "Shoujo Ai", "Shounen", "Shounen Ai", "Spatiu", "Sport",
+                                                                 "SuperPuteri", "Supernatural", "Vampiri", "Yaoi", "Armata", "Psihologic", "Seinen",
+                                                                 "Slice of Life", "Thriller" };
+    }
+}
