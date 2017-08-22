70b08944a21d4b03d055fd0c39eaea15cde1771f
PhoenixItachi <juventus09deva@yahoo.com>
2017-08-22 18:33:00 +0300

    - Changes to the anime Pop-Up/Partial - Created the multi-tab css class for the anime/episode partials.

--- AnimeCentralWeb/Models/DomainViewModels/AnimeViewModel.cs
+++ AnimeCentralWeb/Models/DomainViewModels/AnimeViewModel.cs
@@ -12,6 +12,7 @@ namespace AnimeCentralWeb.Models.DomainViewModels
         [Required]
         public string Title { get; set; }
         [Required]
+        [Display(Name = "Episoade")]
         public int NoOfEpisodes { get; set; }
         public string EpisodeLength { get; set; }
         [Required]
@@ -24,8 +25,10 @@ namespace AnimeCentralWeb.Models.DomainViewModels
         [Required]
         public string Image { get; set; }
         public string BigImage { get; set; }
+        [Display(Name = "Sinonime")]
         public string Synonyms { get; set; }
         [Required]
+        [Display(Name = "Rezumat")]
         public string Synopsis { get; set; }
         [Required]
         public string Genres { get; set; }
