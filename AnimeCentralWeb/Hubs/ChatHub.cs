ac300c293166bfe67c3f6f31920729ecad6ae312
PhoenixItachi <juventus09deva@yahoo.com>
2017-09-09 18:13:25 +0300

    - Added Chat.

--- AnimeCentralWeb/Hubs/ChatHub.cs
+++ AnimeCentralWeb/Hubs/ChatHub.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Hubs;
+using System;
 using System.Threading.Tasks;
 
 namespace AnimeCentralWeb.Hubs
@@ -13,26 +14,17 @@ namespace AnimeCentralWeb.Hubs
     {
         private UserManager<ApplicationUser> UserManager;
         private IHttpContextAccessor HttpContextAccessor;
-        private AnimeCentralDbContext Context;
-        public ChatHub(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor, AnimeCentralDbContext context)
+        public ChatHub(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor)
         {
             UserManager = userManager;
             HttpContextAccessor = httpContextAccessor;
-            Context = context;
         }
         
-        public async void join() {
-            var user = await UserManager.GetUserAsync(HttpContextAccessor.HttpContext.User);
-            if(user != null)
-                Clients.All.broadcastMessage(new { username = user.UserName, image = user.Image}, $"{user.UserName} s-a logat.");
-        }
-
-        public async Task send(string message)
+        public async void send(string message)
         {
             var user = await UserManager.GetUserAsync(HttpContextAccessor.HttpContext.User);
-
             if (user != null)
-                Clients.All.broadcastMessage(new { username = user.UserName, image = user.Image }, message);
+                Clients.All.broadcastMessage(new { username = user.UserName, image = user.Image != null ? user.Image.ToString() : "images/DefaultAvatar.jpg" }, new { content = message, date = DateTime.Now.ToString("d/M/yyyy hh:mm")});
         }
     }
 }
--- AnimeCentralWeb/wwwroot/js/chat.js
+++ AnimeCentralWeb/wwwroot/js/chat.js
@@ -1,19 +1,52 @@
 ﻿var hub;
 $(function () {
   // Chat
+
+  var chat = $(".chat.chat-signalR");
+  var messageBox = $(chat).find(".messages-box");
+  $(chat).showSpinner();
   var connection = $.hubConnection();
   hub = connection.createHubProxy('chat');
   var broadcastMessage = function (user, msg) {
     if (user != null) {
-      console.log(user);
-      $("<p>").text(msg).appendTo(".chat-signalR");
+      newMessage(user, msg).appendTo(messageBox);
     }
   }
 
   hub.on('broadcastMessage', broadcastMessage);
-  connection.start(function () {
-    hub.invoke("join");
+  connection.start().done(function () {
+    $(chat).hideSpinner();
   });
-  console.log("connection started");
-})
+});
 
+$(document).on("keypress", ".message-form .message-input", function (e) {
+  var form = $(this).closest(".message-form");
+  if (form == null || form == undefined)
+    return;
+
+  if (e.which == 13) {
+    $(form).submit();
+    $(this).val("");
+    e.preventDefault();
+  }
+});
+
+
+$(document).on("submit", ".chat-box .message-form", function (e) {
+  e.preventDefault();
+  var messageInput = $(this).find(".message-input");
+  if (messageInput.length != 0 && $(messageInput).val() != "") {
+    hub.invoke('send', $(messageInput).val());
+  }
+});
+
+function newMessage(user, message) {
+  return $("<div class='message-entry'>"
+    + "<div class='message-date'>" + message.date + "</div>"
+    + "<div class='message-user'>"
+    + "<img src='" + user.image + "' class='profile-pic' />"
+    + "<div class='user-name'>" + user.username + ":</div>"
+    + "</div>"
+    + "<div class='message-content'>" + message.content + "</div>"
+    + "</div>");
+}
